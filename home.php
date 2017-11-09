1f5134f6f8c3027aa7f8b772cd56d5905fea1af0
junniiorferreira <kyrvim@gmail.com>
2017-11-09 18:55:45 -0200

    final do segundo dia

--- home.php
+++ home.php
@@ -1,5 +1,25 @@
 <?php include('header.php') ?>
 	<div class="banner">
+		<div class="banner-box">
+			<div class="banner-form">
+				<form class="login-form">
+					<div class="form-group login-form-title">
+						<label>O que está procurando?</label>
+					</div>
+					<div class="form-group">
+						<input class="form-control" name="search" type="text" placeholder="" />						
+					</div>											
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary">Pesquisar</button>
+					</div>
+				</form>
+				<div class="banner-info">
+					<p class="info-title">Bem-vindo a Rede de Pesquisa e Conhecimento Aplicado</p>
+					<p class="info-content">Este site é uma ferramenta para descoberta de informações relacionadas às pesquisas realizadas na FGV.</p>
+					<p class="info-content">Ele permite colaboração entre pesquisadores de diferentes disciplinas promovendo uma visão integrada dos centros, unidades, pesquisadores e suas áreas de interesse.</p>
+				</div>		
+			</div>
+		</div>
 		<img src="assets/images/banner-home-web.png" id="banner-bg-mobile" src="banner-home-mobile.png" alt="" data-focus-left="0.40" data-focus-top="0.01" data-focus-right="0.59" data-focus-bottom="0.99"/>
 		<img src="assets/images/banner-home-web.png" id="banner-bg-web" alt="" data-focus-left="0.00" data-focus-top="1.00" data-focus-right="1.00" data-focus-bottom="0.01"/>
 	</div>	
